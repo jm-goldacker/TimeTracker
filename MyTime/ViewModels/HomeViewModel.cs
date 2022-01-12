bdfc892a167e3c12d31223891dd04cada7327852
Maximilian Goldacker <goldacker.johann@outlook.de>
2022-01-12 21:31:06 +0100

    home view with graph

--- MyTime/ViewModels/HomeViewModel.cs
+++ MyTime/ViewModels/HomeViewModel.cs
@@ -1,4 +1,8 @@
-﻿using MyTime.Helpers;
+﻿using LiveCharts;
+using LiveCharts.Wpf;
+using MyTime.Helpers;
+using MyTime.Models;
+using MyTime.Models.Database;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,7 +11,32 @@ using System.Threading.Tasks;
 
 namespace MyTime.ViewModels
 {
-    class HomeViewModel : Observerable
+    public class HomeViewModel : Observerable
     {
+        private DatabaseContext _context = new DatabaseContext();
+        public HomeViewModel()
+        {
+            var workTimes = _context.WorkTimes.ToList();
+            var workTimesPerDay = workTimes
+                .GroupBy(wt => wt.Start.Date)
+                .Select(g => new { Date = g.Key, TotalHours = g.Sum(x => x.Duration.TotalHours) })
+                .ToDictionary(d => d.Date, d => d.TotalHours);
+
+            SeriesCollection = new SeriesCollection()
+            {
+                new LineSeries()
+                {
+                    Title = "work time",
+                    Values = new ChartValues<double> (workTimesPerDay.Values)
+                }
+            };
+
+            Labels = workTimesPerDay.Keys.Select(k => k.ToString()).ToArray();
+            YFormatter = value => value.ToString("F1");
+        }
+
+        public SeriesCollection SeriesCollection { get; set; }
+        public string[] Labels { get; set; }
+        public Func<double, string> YFormatter { get; set; }
     }
 }
